ca5babac992a9b7b5eb7b4b3a89b257ffa0281c9
Daniel <danielbarral@gmail.com>
2016-11-24 01:21:25 -0200

    Getting json from the real API.

--- src/main/java/com/danielbarral/eircode/controller/EircodeController.java
+++ src/main/java/com/danielbarral/eircode/controller/EircodeController.java
@@ -3,6 +3,7 @@ package com.danielbarral.eircode.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EircodeController {
@@ -12,7 +13,11 @@ public class EircodeController {
     		@PathVariable(value = "key") String key,
     		@PathVariable(value = "search") String search) {
 		
-        return "irishLookup key " + key + " search " + search;
+		String url = "http://ws.postcoder.com/pcw/PCW45-12345-12345-1234X/address/ie/D02X285?lines=3&format=json";
+		RestTemplate restTemplate = new RestTemplate();
+		String json = restTemplate.getForObject(url, String.class);
+		
+        return json;
     }
 	
 	@RequestMapping("/cache/{key}/address/uk/{search}")
