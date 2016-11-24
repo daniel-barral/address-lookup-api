d43aea0bbb594936b471223bae4bcd1357cba5d6
Daniel <danielbarral@gmail.com>
2016-11-24 01:46:34 -0200

    Returning List<Address> instead of String.

--- src/main/java/com/danielbarral/eircode/controller/EircodeController.java
+++ src/main/java/com/danielbarral/eircode/controller/EircodeController.java
@@ -1,23 +1,33 @@
 package com.danielbarral.eircode.controller;
 
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.danielbarral.eircode.model.Address;
+
 @RestController
 public class EircodeController {
 	
 	@RequestMapping("/cache/{key}/address/ie/{search}")
-    public String irishLookup(
+    public List<Address> irishLookup(
     		@PathVariable(value = "key") String key,
     		@PathVariable(value = "search") String search) {
 		
-		String url = "http://ws.postcoder.com/pcw/PCW45-12345-12345-1234X/address/ie/D02X285?lines=3&format=json";
-		RestTemplate restTemplate = new RestTemplate();
-		String json = restTemplate.getForObject(url, String.class);
+		//String url = "http://ws.postcoder.com/pcw/PCW45-12345-12345-1234X/address/ie/D02X285?lines=3&format=json";
+		String url = "http://localhost:8080/pcw/key/address/ie/search";
 		
-        return json;
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<List<Address>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Address>>(){});
+		List<Address> addressList = response.getBody();
+		
+        return addressList;
     }
 	
 	@RequestMapping("/cache/{key}/address/uk/{search}")
